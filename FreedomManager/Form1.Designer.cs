aa4a355958272bd71dae49adeb8375820c90a1f9
Kuborros <kubo@catbat.rocks>
2022-10-12 00:38:09 +0200

    Add project files.

---
+++ FreedomManager/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace FreedomManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.savePlay = new System.Windows.Forms.Button();
+            this.setup = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.modInstall = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // savePlay
+            // 
+            this.savePlay.Location = new System.Drawing.Point(12, 598);
+            this.savePlay.Name = "savePlay";
+            this.savePlay.Size = new System.Drawing.Size(119, 23);
+            this.savePlay.TabIndex = 1;
+            this.savePlay.Text = "Save and Play";
+            this.savePlay.UseVisualStyleBackColor = true;
+            this.savePlay.Click += new System.EventHandler(this.savePlay_Click);
+            // 
+            // setup
+            // 
+            this.setup.Location = new System.Drawing.Point(137, 598);
+            this.setup.Name = "setup";
+            this.setup.Size = new System.Drawing.Size(281, 23);
+            this.setup.TabIndex = 2;
+            this.setup.Text = "Setup BepInEx";
+            this.setup.UseVisualStyleBackColor = true;
+            this.setup.Click += new System.EventHandler(this.setup_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(424, 598);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(75, 23);
+            this.exit.TabIndex = 3;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // modInstall
+            // 
+            this.modInstall.Location = new System.Drawing.Point(12, 525);
+            this.modInstall.Name = "modInstall";
+            this.modInstall.Size = new System.Drawing.Size(119, 67);
+            this.modInstall.TabIndex = 4;
+            this.modInstall.Text = "Install Mod";
+            this.modInstall.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 633);
+            this.Controls.Add(this.modInstall);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.setup);
+            this.Controls.Add(this.savePlay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button savePlay;
+        private System.Windows.Forms.Button setup;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Button modInstall;
+    }
+}
+
---
+++ FreedomManager/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FreedomManager
+{
+    public partial class Form1 : Form
+    {
+        bool bepisPresent = false;
+        bool fp2Found = false;
+        public Form1()
+        {
+            InitializeComponent();
+            bepisPresent = File.Exists("winhttp.dll");
+            fp2Found = File.Exists("FP2.exe");
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void savePlay_Click(object sender, EventArgs e)
+        {
+            if(fp2Found) Process.Start("FP2.exe");
+        }
+
+        private void setup_Click(object sender, EventArgs e)
+        {
+            //TODO: Window with info here.
+            if (!bepisPresent) {
+                WebClient client = new WebClient();
+                client.DownloadFile(new Uri("https://github.com/BepInEx/BepInEx/releases/download/v5.4.21/BepInEx_x86_5.4.21.0.zip"), "BepInEx.zip");
+                //TODO: Check download?
+                ZipFile.ExtractToDirectory("BepInEx.zip", ".");
+                File.Delete("BepInEx.zip");
+            } 
+            else
+            {
+                File.Delete("winhttp.dll");
+                bepisPresent = false;
+            }
+        }
+    }
+}
