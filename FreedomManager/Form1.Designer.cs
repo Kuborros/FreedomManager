c14584481412233834fb76c6fd04275bdd4bc772
Kuborros <kubo@catbat.rocks>
2022-10-13 00:33:10 +0200

    Release

--- FreedomManager/Form1.Designer.cs
+++ FreedomManager/Form1.Designer.cs
@@ -28,16 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.savePlay = new System.Windows.Forms.Button();
             this.setup = new System.Windows.Forms.Button();
             this.exit = new System.Windows.Forms.Button();
             this.modInstall = new System.Windows.Forms.Button();
             this.modFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.refresh = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // savePlay
             // 
-            this.savePlay.Location = new System.Drawing.Point(12, 598);
+            this.savePlay.Location = new System.Drawing.Point(12, 441);
             this.savePlay.Name = "savePlay";
             this.savePlay.Size = new System.Drawing.Size(119, 23);
             this.savePlay.TabIndex = 1;
@@ -47,7 +50,7 @@
             // 
             // setup
             // 
-            this.setup.Location = new System.Drawing.Point(137, 598);
+            this.setup.Location = new System.Drawing.Point(137, 441);
             this.setup.Name = "setup";
             this.setup.Size = new System.Drawing.Size(281, 23);
             this.setup.TabIndex = 2;
@@ -57,7 +60,7 @@
             // 
             // exit
             // 
-            this.exit.Location = new System.Drawing.Point(424, 598);
+            this.exit.Location = new System.Drawing.Point(424, 441);
             this.exit.Name = "exit";
             this.exit.Size = new System.Drawing.Size(75, 23);
             this.exit.TabIndex = 3;
@@ -67,7 +70,7 @@
             // 
             // modInstall
             // 
-            this.modInstall.Location = new System.Drawing.Point(12, 525);
+            this.modInstall.Location = new System.Drawing.Point(12, 368);
             this.modInstall.Name = "modInstall";
             this.modInstall.Size = new System.Drawing.Size(119, 67);
             this.modInstall.TabIndex = 4;
@@ -80,17 +83,38 @@
             this.modFileDialog.DefaultExt = "zip";
             this.modFileDialog.Title = "Select mod to install.";
             // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(406, 350);
+            this.treeView1.TabIndex = 5;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // refresh
+            // 
+            this.refresh.Location = new System.Drawing.Point(137, 368);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(116, 67);
+            this.refresh.TabIndex = 6;
+            this.refresh.Text = "Refresh";
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(511, 633);
+            this.ClientSize = new System.Drawing.Size(511, 475);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.treeView1);
             this.Controls.Add(this.modInstall);
             this.Controls.Add(this.exit);
             this.Controls.Add(this.setup);
             this.Controls.Add(this.savePlay);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "FreedomInstaller";
             this.ResumeLayout(false);
 
         }
@@ -101,6 +125,8 @@
         private System.Windows.Forms.Button exit;
         private System.Windows.Forms.Button modInstall;
         private System.Windows.Forms.OpenFileDialog modFileDialog;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button refresh;
     }
 }
 
--- FreedomManager/Form1.cs
+++ FreedomManager/Form1.cs
@@ -10,6 +10,8 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+using File = System.IO.File;
 
 namespace FreedomManager
 {
@@ -35,7 +37,57 @@ namespace FreedomManager
                 Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
                 savePlay.Hide(); 
             }
+
+            treeView1.Nodes.Add("Mods:");
+            treeView1.Nodes.Add("Mods (Loose DLL):");
+            if (Directory.Exists("Mods"))
+            {
+                treeView1.Nodes.Add("External loader mods (Melons):");
+            }
+
+            if (bepisPresent)
+            {
+                DirectoryScan();
+            }
+
         }
+
+        public bool CheckArchive(String path)
+        {
+            bool hasBepDir = false;
+            if (File.Exists(path) && Path.GetExtension(path) == ".zip")
+            {
+                ZipArchive zipArchive = ZipFile.OpenRead(path);
+                foreach (ZipArchiveEntry zipArchiveEntry in zipArchive.Entries)
+                {
+                    if (zipArchiveEntry.Name == "BepInEx") hasBepDir = true;
+                }
+            }
+            return hasBepDir;
+        }
+
+        public void DirectoryScan()
+        {
+            String dir = "BepInEx\\plugins";
+            try
+            {
+                foreach (string f in Directory.GetFiles(dir))
+                {
+                    treeView1.Nodes[1].Nodes.Add(Path.GetFileName(f));
+                    Console.WriteLine(f);
+                }
+                foreach (string d in Directory.GetDirectories(dir))
+                {
+                    treeView1.Nodes[0].Nodes.Add(Path.GetFileName(d));
+                    Console.WriteLine(d);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }   
+
 
         private void exit_Click(object sender, EventArgs e)
         {
@@ -49,18 +101,26 @@ namespace FreedomManager
 
         private void setup_Click(object sender, EventArgs e)
         {
-            //TODO: Window with info here.
             if (!bepisPresent) {
                 WebClient client = new WebClient();
                 client.DownloadFile(new Uri("https://github.com/BepInEx/BepInEx/releases/download/v5.4.21/BepInEx_x86_5.4.21.0.zip"), "BepInEx.zip");
                 //TODO: Check download?
                 ZipFile.ExtractToDirectory("BepInEx.zip", ".");
                 File.Delete("BepInEx.zip");
+
+                MessageBox.Show(this, "BepInEx installed!.\n\n" +
+                "The game is now ready for modding.",
+                Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
             } 
             else
             {
                 File.Delete("winhttp.dll");
                 bepisPresent = false;
+
+                MessageBox.Show(this, "BepInEx hook removed!.\n\n" +
+                "The mods will no longer be loaded.",
+                Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
             }
         }
 
@@ -68,14 +128,30 @@ namespace FreedomManager
         {
             modFileDialog.ShowDialog();
             string file = modFileDialog.FileName;
-            if (Path.GetExtension(file) == ".zip")
+            if (CheckArchive(file))
             {
                 ZipFile.ExtractToDirectory(file, ".");
                 MessageBox.Show(this, "Mod Unpacked!.\n\n" +
                 "Test Message.",
                 Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DirectoryScan();
             }
+            else
+            {
+                MessageBox.Show(this, "Provided archive is invalid!.\n\n" +
+                "Please ensure the archive has proper directory structure, as well as containing a BepInEx .",
+                Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
 
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            DirectoryScan();
         }
     }
 }
